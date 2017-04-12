d3e5ea28f60be8da62883713e1acd0a4466a49b6
Cody Young <cyoung1623@gmail.com>
2017-04-11 23:37:01 -0700

    Completed ObjectListNodeInterface.

--- src/ObjectListNodeInterface.java
+++ src/ObjectListNodeInterface.java
@@ -2,4 +2,8 @@
  * Created by Cody on 4/11/2017.
  */
 public interface ObjectListNodeInterface {
+    public void setInfo(Object o);
+    public Object getInfo();
+    public void setNext(ObjectListNode p);
+    public ObjectListNode getNext();
 }
