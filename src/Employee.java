f52412b3c5b514a26b8f5b6d1dd7e083347ffe35
codyayoung <cyoung1623@gmail.com>
2017-04-13 15:09:16 -0700

    More completion of instance variables, getters and setters intialized.

--- src/Employee.java
+++ src/Employee.java
@@ -34,48 +34,124 @@ public class Employee implements Comparable {
 
     /**
      * Sets first name of Employee.
-     * @param firstName
+     * @param firstName First name
      */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Compares first names of Employee objects.
+     */
+    public int compareTo (Object j) {
+        Employee i = (Employee) j;
+        return firstName.compareTo(i.getFirstName());
+    }
+
+    /**
+     * Gets last name of Employee.
+     * @return Last name
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Sets last name of Employee.
+     * @param lastName Returns last name of Employee.
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * Compares last names of Employee objects.
+     */
+    public int compareToLast (Object j) {
+        Employee i = (Employee) j;
+        return lastName.compareTo(i.getLastName());
+    }
+
+    /**
+     * Gets gender of Employee.
+     * @return Gender: male/female
+     */
     public String getGender() {
         return gender;
     }
 
+    /**
+     * Sets gender of Employee
+     * @param gender Gender: male/female
+     */
     public void setGender(String gender) {
         this.gender = gender;
     }
 
+    /**
+     * Compares genders of Employee objects.
+     * @param j Employee object to compare
+     * @return 1 if genders match, 0 if not
+     */
+    public int compareGender(Object j) {
+        Employee i = (Employee) j;
+        return gender.compareTo(i.getGender());
+    }
+
+    /**
+     * Gets tenure of Employee object.
+     * @return Tenure in years
+     */
     public int getTenure() {
         return tenure;
     }
 
+    /**
+     * Sets tenure of Employee object.
+     * @param tenure Tenure in years
+     */
     public void setTenure(int tenure) {
         this.tenure = tenure;
     }
 
+    /**
+     * Gets rate of employee.
+     * @return Rate: hourly/weekly
+     */
     public String getRate() {
         return rate;
     }
 
+    /**
+     * Sets rate of employee.
+     * @param rate Rate: hourly/weekly
+     */
     public void setRate(String rate) {
         this.rate = rate;
     }
 
+    /**
+     * Compares rates of Employee objects.
+     * @param j
+     * @return 1 if rates match, 0 if not
+     */
+    public int compareRate(Object j) {
+        Employee i = (Employee) j;
+        return rate.compareTo(i.getRate());
+    }
+
+    /**
+     * Gets salary of Employee objects.
+     * @return Salary in dollars
+     */
     public double getSalary() {
         return salary;
     }
 
+    /**
+     * Sets salary of Employee objects.
+     * @param salary Salary in dollars
+     */
     public void setSalary(double salary) {
         this.salary = salary;
     }
